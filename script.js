a50d6758576aa250fa28ca6b7af0257a30028430
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-11-28 18:52:56 +0100

    Make the game works
    Added the player selection and the comparison of both choices

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let computerChoice = ' '
+let computerChoice = getComputerChoice();
+let playerSelection = window.prompt('What will you play?');
+let result = playRound();
 
-function getComputerChoice(){
+function getComputerChoice(computerChoice){
     let random = Math.floor(Math.random() * 3 + 1);
     switch(random){
         case 1:
@@ -15,3 +17,17 @@ function getComputerChoice(){
     }
 
 }
+
+function playRound(playerSelection, computerChoice, result) {
+    if (playerSelection == computerChoice) {
+        return result = 'its a draw!';
+    } else if ((playerSelection === 'Rock' && computerChoice === 'Scissors') || (playerSelection === 'Scissors' && computerChoice === 'Paper') || (playerSelection === 'Paper' && computerChoice === 'Rock')) {
+        return result = 'Congrats, you won!';
+    } else {
+        return result = 'You lose, try again!';
+    }
+}
+
+console.log(computerChoice);
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerChoice));
