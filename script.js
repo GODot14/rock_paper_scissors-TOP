41abbce2c796055a5d5ebbfbe63717438626dae5
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-11-29 07:50:21 +0100

    Format player selection and check for valid choice

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*TO DO:
+- playRound function for BO5
+- make playerSelection case-insensitive
+- add context to consolelog for better readability
+- do not accept othen than rock paper scissors */
+
 let computerChoice = getComputerChoice();
 let playerSelection = window.prompt('What will you play?');
 let result = playRound();
@@ -28,6 +34,19 @@ function playRound(playerSelection, computerChoice, result) {
     }
 }
 
+function checkPlayerSelection(playerSelection) {
+   playerSelection = playerSelection.toLowerCase();
+   if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+    let firsLetter = playerSelection.slice(0, 1);
+    let remaining = playerSelection.slice(1);
+    let format = firsLetter.toUpperCase();
+    return playerSelection = format + remaining;
+   } else {
+    return 'Choose a real play!';
+   }
+}
+
 console.log(computerChoice);
 console.log(playerSelection);
-console.log(playRound(playerSelection, computerChoice));
+console.log(checkPlayerSelection(playerSelection));
+/*console.log(playRound(playerSelection, computerChoice));*/
