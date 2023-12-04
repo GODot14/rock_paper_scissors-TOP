3c94f15ecebf1e98fe9a0aedd62f8956cc6e3f5a
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-04 18:21:14 +0100

    Trash commit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /*TO DO:
-- playRound function for BO5
-- add context to consolelog for better readability
-- do not accept other than rock paper scissors */
+- playRound function for BO5*/
 
 let computerChoice = getComputerChoice();
-let playerSelection = window.prompt('What will you play?');
+let playerSelection = '';
 let result = playRound();
 
 function getComputerChoice(computerChoice){
@@ -23,7 +21,33 @@ function getComputerChoice(computerChoice){
 
 }
 
+function game() {
+    let countPlayer = 0;
+    let countComputer = 0;
+    let draw = 0;
+    for (let i = 0; i < 5;) {
+        console.log(`You played ${checkPlayerSelection(playerSelection)}`);
+        console.log(`The computer played ${computerChoice}`);
+        playRound();
+        if (result === 'draw') {
+            draw++;
+            console.log('It\'s a draw!');
+        } else if (result === 'playerWin') {
+            countPlayer++;
+            console.log('You win this round!');
+        } else if (result === 'playerLoose') {
+            countComputer++;
+            console.log('You loose this round!');
+        } else {
+            console.log('Choose a real play!');
+        }
+
+        i = countComputer + countPlayer + draw;
+    }
+}
+
 function checkPlayerSelection(playerSelection) {
+    playerSelection = window.prompt('What will you play?');
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
      let firsLetter = playerSelection.slice(0, 1);
@@ -37,17 +61,14 @@ function checkPlayerSelection(playerSelection) {
 
 function playRound(formatPlayerSelection, computerChoice, result) {
     if (formatPlayerSelection === computerChoice) {
-        return result = 'its a draw!';
+        return result = 'draw';
     } else if ((formatPlayerSelection === 'Rock' && computerChoice === 'Scissors') || (formatPlayerSelection === 'Scissors' && computerChoice === 'Paper') || (formatPlayerSelection === 'Paper' && computerChoice === 'Rock')) {
-        return result = 'Congrats, you won!';
+        return result = 'playerWin';
     } else if (formatPlayerSelection === 'error') {
-        return result = 'Choose a real play!';
+        return result = 'error';
     } else {
-        return result = 'You lost, try again!';
+        return result = 'playerLoose';
     }
 }
 
-console.log(`The computer played ${computerChoice}`);
-/*console.log(playerSelection);*/
-console.log(`You played ${checkPlayerSelection(playerSelection)}`);
-console.log(playRound(formatPlayerSelection, computerChoice, result));
+game();
