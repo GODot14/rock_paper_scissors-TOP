7bc57df9c5e756da03dcbf224fdf22649a3a1bb5
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-19 21:53:43 +0100

    bugfix buttons

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const ENUM_CHOICE = {
     ROCK: 'Rock',
     PAPER: 'Paper',
-    SCISSORS: 'Scissors'
+    SCISSORS: 'Scissors',
 }
 const ENUM_STATUS = {
     DRAW: 'draw',
     WIN: 'win',
-    LOOSE: 'loose'
+    LOOSE: 'loose',
 }
 const buttons = document.querySelector('.buttons');
+const resultText = document.querySelector('.resultText');
+
 let playerChoice = null;
 let computerChoice = null;
-const resultText = document.querySelector('.resultText');
+let draw = 0;
+let playerCount = 0;
+let computerCount = 0;
+let totalCount = 0;
 
 buttons.addEventListener('click', (button) => {
     computerChoice = getComputerChoice();
@@ -19,13 +24,18 @@ buttons.addEventListener('click', (button) => {
     switch (playerChoice) {
         case 'rock':
             playerChoice = ENUM_CHOICE.ROCK;
+            playRound(playerChoice, computerChoice);
+            break
         case 'paper':
             playerChoice = ENUM_CHOICE.PAPER;
+            playRound(playerChoice, computerChoice);
+            break
         case 'scissors':
             playerChoice = ENUM_CHOICE.SCISSORS;
-        }
-    playRound(playerChoice, computerChoice);
-    })
+            playRound(playerChoice, computerChoice);
+            break
+    }
+})
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
@@ -42,44 +52,30 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         resultText.textContent = `You both played ${playerSelection}. It\'s a draw!`;
-        /*return ENUM_STATUS.DRAW;*/
+        game(ENUM_STATUS.DRAW);
     } else if ((playerSelection === ENUM_CHOICE.PAPER && computerSelection === ENUM_CHOICE.ROCK)
      || (playerSelection === ENUM_CHOICE.ROCK && computerSelection === ENUM_CHOICE.SCISSORS)
      || (playerSelection === ENUM_CHOICE.SCISSORS && computerSelection === ENUM_CHOICE.PAPER)) {
          resultText.textContent = `You played ${playerSelection} and the computer played ${computerChoice}. You win!`;
-         /*return ENUM_STATUS.WIN;*/
+         game(ENUM_STATUS.WIN);
     } else {
         resultText.textContent = `You played ${playerSelection} and the computer played ${computerChoice}. You loose!`;
-        /*return ENUM_STATUS.LOOSE;*/
+        game(ENUM_STATUS.LOOSE)
     }
 }
 
-/*function game() {
-    let draw = 0;
-    let playerCount = 0;
-    let computerCount = 0;
-    for (let index = 0; index < 5;) {
-        let playerChoice = getPlayerChoice();
-        console.log(`You played ${playerChoice}!`);
-
-        let computerChoice = getComputerChoice();
-        console.log(`The computer played ${computerChoice}!`);
-
-        let result = playRound(playerChoice, computerChoice);
-        if (result === ENUM_STATUS.DRAW) {
-            draw++;
-            console.log('It\'s a draw!');
-        } else if (result === ENUM_STATUS.WIN) {
-            playerCount++;
-            console.log('You win this round!');
-        } else {
-            computerCount++;
-            console.log('You loose this round!');
-        }
-
-        index = computerCount + playerCount + draw;
+function game(status) {
+    if (status === ENUM_STATUS.DRAW) {
+        draw++;
+        console.log('It\'s a draw!');
+    } else if (status === ENUM_STATUS.WIN) {
+        playerCount++;
+        console.log('You win this round!');
+    } else {
+        computerCount++;
+        console.log('You loose this round!');
     }
+
+    totalCount = computerCount + playerCount + draw;
     console.log(`You won ${playerCount} round. The computer won ${computerCount} round. You made ${draw} draw`);
 }
-
-game();*/
